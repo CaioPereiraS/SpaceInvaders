5ea28e5b57487dc2bc78ca50e2d4c051277599c8
kenny <caiopereira.dev@gmail.com>
2023-05-22 18:49:52 -0300

    22/05/2023

--- Controller/Conexao.java
+++ Controller/Conexao.java
@@ -3,8 +3,11 @@ package Controller;
 import java.sql.*;
 public class Conexao {
 
-    public Connection getConnection() throws SQLException {
-        Connection conexao = DriverManager.getConnection("postgresql://postgres:WIBforr7Dwe0jXTswZ1h@containers-us-west-77.railway.app:5542/railway");
-        return conexao;
+    private static final String URL = "jdbc:postgresql://postgres:WIBforr7Dwe0jXTswZ1h@containers-us-west-77.railway.app:5542/railway";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "WIBforr7Dwe0jXTswZ1h";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
--- GameModule/Principal.java
+++ GameModule/Principal.java
@@ -11,20 +11,21 @@ public class Principal {
     public static final int ALTURA_TELA = 600;
 
     //CONSTRUTOR
-    public Principal(){
+    public Principal() {
         JFrame janela = new JFrame("Space Invaders");
         Game game = new Game();
-        game.setPreferredSize(new Dimension(LARGURA_TELA,ALTURA_TELA));
+        game.setPreferredSize(new Dimension(LARGURA_TELA, ALTURA_TELA));
         janela.getContentPane().add(game);
         janela.setResizable(false);
         janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        janela.setLocation(100,100);
+        janela.setLocation(100, 100);
         janela.setVisible(true);
         janela.pack();
 
     }
-   public static void main(String[] args) throws SQLException {
-        UsuarioController Controlador = new UsuarioController();
-        Controlador.cadastrarUsuario();
-   }
+
+//    public static void main(String[] args) throws SQLException {
+//        UsuarioController Controlador = new UsuarioController();
+//        Controlador.cadastrarUsuario();
+//    }
 }
